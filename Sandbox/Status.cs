c83be29f57486816a9ebf08361ae17d3ad0e2105
EASJ <EASJ@EASJ3212>
2016-09-28 20:10:25 +0200

    Add project files.

---
+++ Sandbox/Status.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    // This class could be part of an application similar to Facebook.
+    // The class represents a status that a user can post - other users
+    // can then "like" or "dislike" the status post 
+    public class Status
+    {
+        private string statusText;
+        private int likes;
+        private int dislikes;
+
+        // Create the Status - initially, the status has zero likes
+        // and zero dislikes
+        public Status(string statusText)
+        {
+            this.statusText = statusText;
+            this.likes = 0;
+            this.dislikes = 0;
+        }
+
+        public void AddOneLike()
+        {
+            likes = likes + 1;
+        }
+
+        public void AddOneDislike()
+        {
+            dislikes = dislikes + 1;
+        }
+
+        public string GetStatusText()
+        {
+            return statusText;
+        }
+
+        public int GetNoOfLikes()
+        {
+            return likes;
+        }
+
+        public int GetNoOfDislikes()
+        {
+            return dislikes;
+        }
+    }
+}
