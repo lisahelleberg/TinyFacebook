7c7971b174fb2a022aab3d090481bcbf0eaef0fb
Martin Kierkegaard <mark@easj.dk>
2016-09-29 11:32:16 +0200

    a. Creates a new Status object.

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -7,6 +7,8 @@ namespace Sandbox
         public void MyCode()
         {
             // The FIRST line of code should be BELOW this line
+            Console.WriteLine("Test of part 2...");
+            Status status1 = new Status("I am late for class!! OMG!!");
 
             // The LAST line of code should be ABOVE this line
         }
